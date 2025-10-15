91c3ff64b9a9b94658bce92e72727d5967104b05
AsgerMoellerSpec <asger.moller@specialisterne.com>
2025-10-15 12:42:01 +0200

    Got test project to work with original project!!

---
+++ tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Week_4_PDF_downloader.FileHandler fh = new Week_4_PDF_downloader.FileHandler();
+    }
+}
